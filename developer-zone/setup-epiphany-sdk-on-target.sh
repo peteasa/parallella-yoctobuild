84d54755edb5e531d581c0c7b38b91e8764c86fc
Peter Saunderson <peteasa@gmail.com>
2015-08-09 19:40:00 +0100

    target setup modified to allow same Makefile to be used in yocto build, SDK and on target

--- developer-zone/setup-epiphany-sdk-on-target.sh
+++ developer-zone/setup-epiphany-sdk-on-target.sh
@@ -12,6 +12,13 @@ export EPIPHANY_HOME=/usr/epiphany/epiphany-sdk
 export EPIPHANY_HDF=${EPIPHANY_HOME}/bsps/current/platform.hdf
 export LD_LIBRARY_PATH=/usr/lib/epiphany-elf:/usr/lib:${LD_LIBRARY_PATH}
 
+## EXOTIC_TARGET_SYS is used in yocto Makefiles for epiphany code so define it here 
+export EXOTIC_TARGET_SYS=epiphany-elf
+## yocto Makefiles for epiphany code expect the tools in /usr/bin/epiphany-elf
+mkdir /usr/bin/epiphany-elf
+ln -s /usr/bin/epiphany-elf-gcc /usr/bin/epiphany-elf/gcc
+ln -s /usr/bin/epiphany-elf-objcopy /usr/bin/epiphany-elf/objcopy
+
 ##
 ## The end!
 ##
